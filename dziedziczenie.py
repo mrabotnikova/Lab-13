7931f8fcbb1e1543f6d4554dd8a174174f5e1e5c
mrabotnikova <mrabotnikova@gmail.com>
2025-01-20 19:17:06 +0100

    Dodano klasę Student dziedziczącą z Osoba

--- dziedziczenie.py
+++ dziedziczenie.py
@@ -5,3 +5,13 @@ class Osoba:
 
     def przedstaw_sie(self):
         return f"Nazywam się {self.imie} {self.nazwisko}."
+
+
+class Student(Osoba):
+    def __init__(self, imie, nazwisko, numer_indeksu):
+        super().__init__(imie, nazwisko)
+        self.numer_indeksu = numer_indeksu
+
+    def przedstaw_sie(self):
+        return f"Jestem studentem. {super().przedstaw_sie()} Mój numer indeksu to {self.numer_indeksu}."
+
