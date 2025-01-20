7acaef5040e18d9a3d377db3b45c597e2294f70c
mrabotnikova <mrabotnikova@gmail.com>
2025-01-20 19:21:01 +0100

    Dodano funkcję testującą klasy Student i Wykładowca

--- dziedziczenie.py
+++ dziedziczenie.py
@@ -14,7 +14,7 @@ class Student(Osoba):
 
     def przedstaw_sie(self):
         return f"Jestem studentem. {super().przedstaw_sie()} Mój numer indeksu to {self.numer_indeksu}."
-        
+
 class Wykładowca(Osoba):
     def __init__(self, imie, nazwisko, przedmiot):
         super().__init__(imie, nazwisko)
@@ -22,5 +22,13 @@ class Wykładowca(Osoba):
 
     def przedstaw_sie(self):
         return f"Jestem wykładowcą. {super().przedstaw_sie()} Prowadzę zajęcia z {self.przedmiot}."
+
+if __name__ == "__main__":
+    student = Student("Jan", "Kowalski", "12345")
+    wykladowca = Wykładowca("Anna", "Nowak", "Matematyka")
+
+    print(student.przedstaw_sie())
+    print(wykladowca.przedstaw_sie())
+
 
 
